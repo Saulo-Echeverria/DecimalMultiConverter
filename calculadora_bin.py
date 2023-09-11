c7ac1181266bc6bfaf199671f0b95b72d8139eb9
Saulo-Echeverria <saulogomes4@hotmail.com>
2023-09-10 22:50:37 -0300

    incluida função de loop

--- calculadora_bin.py
+++ calculadora_bin.py
@@ -1,22 +1,31 @@
 def decimal_to_binary(n):
-    if n == 0:
-        return "0"
-    else:
-        return str(bin(n))[2:]
+  if n == 0:
+    return "0"
+  else:
+    return str(bin(n))[2:]
+
 
 def decimal_to_octal(n):
-    if n == 0:
-        return "0"
-    else:
-        return str(oct(n))[2:]
+  if n == 0:
+    return "0"
+  else:
+    return str(oct(n))[2:]
+
 
 def decimal_to_hexadecimal(n):
-    if n == 0:
-        return "0"
-    else:
-        return hex(n)[2:]
+  if n == 0:
+    return "0"
+  else:
+    return hex(n)[2:]
 
-n = int(input("Digite um número decimal: "))
-print("Binário: ", decimal_to_binary(n))
-print("Octal: ", decimal_to_octal(n))
-print("Hexadecimal: ", decimal_to_hexadecimal(n))
+
+while True:  # Loop infinito
+    n = int(input("Digite um número decimal (ou -1 para sair): "))
+    
+    if n == -1:
+        print("Programa encerrado.")
+        break  # Sai do loop
+    
+    print("Binário: ", decimal_to_binary(n))
+    print("Octal: ", decimal_to_octal(n))
+    print("Hexadecimal: ", decimal_to_hexadecimal(n))
